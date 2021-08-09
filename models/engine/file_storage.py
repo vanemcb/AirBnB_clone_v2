56e4da536eeb57fc81a760df25e413da4454d207
imac_esneider <sneidergv@gmail.com>
2021-08-09 23:49:31 +0000

    03 | models/engine/file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,7 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         return FileStorage.__objects
 
@@ -36,15 +36,29 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
+            pass
+
+    def delete(self, obj=None):
+        # If obj=None try block code won't execute
+        try:
+            # parse the obj key as will be stored in __objects
+            key = obj.to_dict()['__class__'] + '.' + obj.id
+
+            # delete an specify object with the key "key" from __objects
+            FileStorage.__objects.pop(key)
+
+            # Save changes in a json file
+            self.save()
+        except:
             pass
