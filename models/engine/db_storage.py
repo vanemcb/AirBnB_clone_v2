69e94f46bc45a1e0203f0fcf57190e8eb5add006
vanemcb <vanem2201@gmail.com>
2021-08-11 11:12:42 -0500

    Functions all, new, save, delete, reload created

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,11 +5,9 @@
 import os
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.log import instance_logger
-from sqlalchemy.orm import Session, query
+from sqlalchemy.orm import Session, query, scoped_session
 from sqlalchemy.orm.session import sessionmaker
-# from models import user, state, city, amenity, place, review
 
-# os.environ[env_var] = env_var_value
 
 os.environ.update({
     "HBNB_ENV": "dev",
@@ -37,23 +35,21 @@ class DBStorage:
     __session = None
 
     def __init__(self):
-        """ Pass """
+        """ Intance Method """
         self.__engine = create_engine(
             'mysql+mysqldb://{}:{}@{}/{}'.format(
                 HBNB_MYSQL_USER,
                 HBNB_MYSQL_PWD,
                 HBNB_MYSQL_HOST,
                 HBNB_MYSQL_DB), pool_pre_ping=True)
+
         if os.getenv("HBNB_ENV") == "test":
-            # Session = sessionmaker(self.__engine)
-            # session = Session()
-            ##################
             meta = MetaData(self.__engine)
             meta.reflect()
             meta.drop_all()
 
     def all(self, cls=None):
-        from models.base_model import BaseModel
+        """Returns a dictionary of models currently in storage"""
         from models.user import User
         from models.place import Place
         from models.state import State
@@ -61,18 +57,51 @@ class DBStorage:
         from models.amenity import Amenity
         from models.review import Review
 
-        classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
-        self.__session = Session(self.__engine)
-        list_objects = self.__session.query(cls).all()
+        classes = {'State': State, 'City': City}
+
         query_dict = {}
-        for obj in list_objects:
-            query_dict.update({str(cls.__name__) + "." + obj.id: obj})
+
+        if cls is None:
+            for value in classes.values():
+                for obj in self.__session.query(value).all():
+                    query_dict.update(
+                        {obj.__class__.__name__ + "." + obj.id: obj})
+        else:
+            for obj in self.__session.query(cls).all():
+                query_dict.update(
+                    {obj.__class__.__name__ + "." + obj.id: obj})
+
+        self.__session.close()
         return query_dict
 
+    def new(self, obj):
+        """Method that adds the object to the current database session"""
+        self.__session.add(obj)
+        self.__session.close()
 
-instance = DBStorage()
-instance.all()
+    def save(self):
+        """ Method that commits all changes of the current database session """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ Method that delete from the current database session
+        obj if not None"""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """ Method that creates all tables in the database """
+        from models.base_model import BaseModel, Base
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        Base.metadata.create_all(self.__engine)
+
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
