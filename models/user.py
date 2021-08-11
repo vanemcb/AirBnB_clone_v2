c94c8f5f8c7ad53efaf6ce1799271f9f1ba675a0
imac_esneider <sneidergv@gmail.com>
2021-08-11 21:09:50 +0000

    8. DBStorage - Place

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relation, relationship
 
 
 class User(BaseModel, Base):
@@ -11,3 +12,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship(
+        "Place", backref="user", cascade="all, delete", passive_deletes=True)
