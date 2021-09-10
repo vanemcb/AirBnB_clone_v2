cad70f000ff52c65c6d375013e93811106da23b8
vanemcb <vanem2201@gmail.com>
2021-09-10 11:35:30 -0500

    New project created

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Function that displays “Hello HBNB!” """
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(debug=True, port='5000', host='0.0.0.0')
