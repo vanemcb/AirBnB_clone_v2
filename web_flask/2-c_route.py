c01e7914acb82b3538e8d97765272315628e9040
vanemcb <vanem2201@gmail.com>
2021-09-10 12:03:23 -0500

    Script created

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,5 +17,13 @@ def display_hbnb():
     """ Function that displays “HBNB” """
     return 'HBNB'
 
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """ Function that displays “C” followed by the value of
+    the text variable """
+    return 'C {}'.format(text.replace("_", " "))
+
+
 if __name__ == '__main__':
     app.run(debug=True, port='5000', host='0.0.0.0')
