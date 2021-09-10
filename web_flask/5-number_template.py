cc6e69ec4e25f07ac452932a624def70dd632ba2
vanemcb <vanem2201@gmail.com>
2021-09-10 14:29:58 -0500

    Script created

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ script that starts a Flask web application """
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -43,7 +43,7 @@ def display_number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_number_template(n):
     """ Function that displays a HTML page only if n is an integer """
-    return '{} is a number'.format(n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
