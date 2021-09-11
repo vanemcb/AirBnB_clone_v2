43a27f3ce80cac12d3337afd14e3856e6f1fc361
vanemcb <vanem2201@gmail.com>
2021-09-11 18:01:44 -0500

    Method close fixed

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -104,4 +104,4 @@ class DBStorage:
     def close(self):
         """ Calls remove() method on the private session attribute
         (self.__session)  or close() on the class Session """
-        self.__session.remove()
+        self.__session.close()
