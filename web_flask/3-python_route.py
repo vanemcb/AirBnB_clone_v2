41158297b3435eb53435ec042ad610b59bee868b
vanemcb <vanem2201@gmail.com>
2021-09-11 17:24:50 -0500

    string

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,7 +26,7 @@ def display_c(text):
 
 
 @app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
 def display_python(text='is cool'):
     """ Function that displays “Python ”, followed by the value
     of the text variable  """
